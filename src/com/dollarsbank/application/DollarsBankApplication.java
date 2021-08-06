93a22090a5d36923b20a582bc2582eea6bd8d724
Nikita Tran <nikiusa09@yahoo.com>
2021-08-06 14:36:47 -0400

    added colorutil, connectionmanager, gitignore

---
+++ src/com/dollarsbank/application/DollarsBankApplication.java
@@ -0,0 +1,12 @@
+package com.dollarsbank.application;
+
+import com.dollarsbank.utility.ColorsUtil;
+
+public class DollarsBankApplication {
+
+	public static void main(String[] args) {
+		//System.out.println(ColorsUtil.ANSI_RED + "This text is red!" + ColorsUtil.ANSI_RESET);
+
+	}
+
+}
